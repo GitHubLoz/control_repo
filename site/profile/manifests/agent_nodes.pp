4be2e8424188c524d2ddb98d0bd85c8565a5d5c8
GitHubLoz <lawrence.arkle@gmail.com>
2020-08-12 14:42:07 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragrent::node {'web.puppet.vm':}
-  dockeragrent::node {'db.puppet.vm':}
+  dockeragent::node {'web.puppet.vm':}
+  dockeragent::node {'db.puppet.vm':}
   }
